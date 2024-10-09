d9d3be715fda95c1b93cfa0b58457e5ce941426e
Shawn Dahm <biker896@hotmail.com>
2024-10-09 14:55:58 -0700

    added more javascript

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ console.log('Hello Shawn'+ username);
 let time = prompt('What time is it');
 console.log('Time is' + time);
 
-document.write('Hello ' + username + '  coffee time is at '+ time);
+let favoriteRoast = prompt('What is your favorite roast coffee?');
+console.log ('Favortie roast coffee is'+ username + time)
+document.write('Hello ' + username +' ' + favoriteRoast + '  roast coffee time is at '+ time);
