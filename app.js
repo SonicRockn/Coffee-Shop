8e7e6be704a007c16ed58b1e9810477ced40cc5d
Shawn Dahm <biker896@hotmail.com>
2024-10-09 14:19:35 -0700

    added more language

--- app.js
+++ app.js
@@ -3,4 +3,8 @@
 let username = prompt('What is your name?');
 console.log('Hello Shawn'+ username);
 
-document.write('Hello ' + username + '!!');
+
+let time = prompt('What time is it');
+console.log('Time is' + time);
+
+document.write('Hello ' + username + '  coffee time is at '+ time);
