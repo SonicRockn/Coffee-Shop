703bbc36f61a7edcd00a52fc6eed37c33d5c009f
Shawn Dahm <biker896@hotmail.com>
2024-10-15 16:55:43 -0700

    added more java and image sequence based on input

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 'use strict';
 
-let username = prompt('What is your name?');
-console.log('Hello Shawn'+ username);
+
+function helloUser() {
+    let username = prompt('What is your name?');
+    console.log('Hello ' + username);
+    document.write('Hello ' + username );
+}
+helloUser();
+
+function time() {
+    let currentTime = prompt('What time is it?');
+    console.log('Time is ' + currentTime);
+    document.write('Time is ' + currentTime); 
+}
+time();
+
+function favorite() {
+    let favoriteRoast = prompt('What is your favorite roast coffee?');
+    console.log('Favorite roast coffee is ' + favoriteRoast);
+    document.write(favoriteRoast + ' roast all the time');
+}
+favorite();
+
+let userInput = '';
+while (userInput !== 'order') {
+    userInput = prompt("Type 'order' to end the loop");
+    console.log('You typed ' + userInput);
+    document.write('You typed: ' + userInput );
+}
 
 
-let time = prompt('What time is it');
-console.log('Time is' + time);
+let numImages = prompt('How many coffees would you like?');
 
-let favoriteRoast = prompt('What is your favorite roast coffee?');
-console.log ('Favortie roast coffee is'+ username + time)
-document.write('Hello ' + username +' ' + favoriteRoast + '  roast coffee time is at '+ time);
+numImages = parseInt(numImages);
+
+if (!isNaN(numImages) && numImages > 0) {
+    for (let i = 0; i < numImages; i++) {
+        const container = document.getElementById('image-container');
+        const imageUrl = "https://clipart.com/thumbs.php?f=/747/batch_01/000747-0001-000089_tnb.png";
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        img.alt = 'Repeated Image';
+        img.style.width = '100px'; 
+        img.style.height = '100px'; 
+        container.appendChild(img);
+    }
+} 
